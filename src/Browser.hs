656cd320997136e6a810a0ad5659c1fcfb57a597
Michael F. Schönitzer <michael@schoenitzer.de>
2017-09-01 02:35:01 +0200

    Aussehen und Kleinigkeiten

--- src/Browser.hs
+++ src/Browser.hs
@@ -49,10 +49,11 @@ instance YesodPersist Browser where
 
 getHomeR :: Handler Html
 getHomeR = do
-    results <- runDB $ selectList []  [LimitTo 100]
+    results <- runDB $ selectList []  [LimitTo 50]
     defaultLayout $ do
        setTitle "Floss-Browser"
        toWidget $(hamletFile "./templates/softwarelist.hamlet")
+       toWidget $(luciusFile "./templates/softwarelist.lucius")     -- TODO
 
 getSoftwareR :: String -> Handler Html
 getSoftwareR software = do
@@ -62,29 +63,34 @@ getSoftwareR software = do
       setTitle $ toHtml $ software ++ " "         -- TODO
       --toWidget $(luciusFile "./foo.lucius")     -- TODO
       toWidget $(hamletFile "./templates/software.hamlet")
+      toWidget $(luciusFile "./templates/softwarelist.lucius")     -- TODO
 
 getByLicenseR :: Int -> Handler Html
 getByLicenseR license = do
     results <- runDB
-           $ E.select
+           $ E.select $ E.distinct
            $ E.from $ \(p `E.InnerJoin` pl) -> do
                 E.on $ p ^. ProjectId E.==. pl ^. ProjectLicenseFkProjectId
                 E.where_ ( pl ^. ProjectLicenseFkLicenseId E.==. E.val (qidtokey license) )
+                E.limit 50
                 return p
     defaultLayout $ do
        toWidget $(hamletFile "./templates/softwarelist.hamlet")
+       toWidget $(luciusFile "./templates/softwarelist.lucius")     -- TODO
        setTitle "Floss-Browser"
 
 getByCodingR :: Int -> Handler Html
 getByCodingR coding = do
     results <- runDB
-           $ E.select
+           $ E.select $ E.distinct
            $ E.from $ \(p `E.InnerJoin` pc) -> do
                 E.on $ p ^. ProjectId E.==. pc ^. ProjectCodingFkProjectId
                 E.where_ ( pc ^. ProjectCodingFkCodingId E.==. E.val (qidtokey coding) )
+                E.limit 50
                 return p
     defaultLayout $ do
        toWidget $(hamletFile "./templates/softwarelist.hamlet")
+       toWidget $(luciusFile "./templates/softwarelist.lucius")     -- TODO
        setTitle "Floss-Browser"
 
 
